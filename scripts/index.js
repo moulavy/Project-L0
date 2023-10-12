6ad60ec96c3942655a80d8b3aa1ddab2f7272c3c
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-10-13 00:45:33 +0300

    переделала в template product

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
+import { initialProducts } from "./products.js";
 document.addEventListener('DOMContentLoaded', function () {
    let likeButtons = document.querySelectorAll('.product__like');
-   let deleteButtons = document.querySelectorAll('.product__delete');
    let products = document.querySelectorAll('.product');
 
    products.forEach(function (product) {
+
       let deleteButton = product.querySelector('.product__delete');
       deleteButton.addEventListener('click', function () {
          product.classList.toggle('delete');
       });
-      console.log('ff')
    });
 
    likeButtons.forEach(function (button) {
@@ -16,4 +16,63 @@ document.addEventListener('DOMContentLoaded', function () {
          button.classList.toggle('active');
       });
    });
+
+   const productsList = document.querySelector('.products-list');
+   const productTemplate = document.querySelector('.product__template').content;
+
+   function toPrice(value) {
+      let arr = value.split('');
+      let initialLength = arr.length;
+      for (let i = arr.length - 1; i >= 0; i--) {
+         if ((initialLength - i) % 3 === 0 && i != 0) {
+            arr.splice(i, 0, ' ');
+         }
+      }
+      return arr.join('');
+   }
+   initialProducts.forEach(function (productItem) {
+      let productElement = productTemplate.querySelector('.product').cloneNode(true);
+      let productElementTitle = productElement.querySelector('.product__title');
+      let productElementImg = productElement.querySelector('.product__img');
+      let productElementColor = productElement.querySelector('.product__color');
+      let productElementSize = productElement.querySelector('.product__size');
+      let productElementStorage = productElement.querySelector('.product__storage');
+      let productElementCompany = productElement.querySelector('.product__company-text');
+      let productElementInput = productElement.querySelector('.product__quantity-field');
+
+      let productElementPriceDesktop = productElement.querySelector('.product__prices')
+      let productElementActivePrice = productElementPriceDesktop.querySelector('.product__active-price-value');
+      let productElementOldPrice = productElementPriceDesktop.querySelector('.product__old-price-value');
+      let productElementPriceMobile = productElement.querySelector('.product__prices-mobile')
+      let productElementActivePriceMobile = productElementPriceMobile.querySelector('.product__active-price-value');
+      let productElementOldPriceMobile = productElementPriceMobile.querySelector('.product__old-price-value');
+      let productElementRemains = productElement.querySelector('.product__remains');
+
+      productElementTitle.textContent = productItem.name;
+      productElementImg.src = productItem.img;
+      if (productItem.color != '') {
+         productElementColor.querySelector('.product__param-key').textContent = 'Цвет: ';
+         productElementColor.querySelector('.product__param-value').textContent = productItem.color;
+      }
+      if (productItem.size != '') {
+         productElementSize.querySelector('.product__param-key').textContent = 'Размер: ';
+         productElementSize.querySelector('.product__param-value').textContent = productItem.size;
+      }
+      productElementStorage.textContent = productItem.storage;
+      productElementCompany.textContent = productItem.company;
+      productElementInput.value = productItem.count;
+      let priceActiveValue = toPrice((productItem.activePrice * productItem.count).toString());
+      let priceOldValue = toPrice((productItem.oldPrice * productItem.count).toString());
+      if (priceActiveValue.length > 6) {
+         productElementActivePrice.classList.add('product__long-price');
+      }
+      productElementActivePrice.textContent = priceActiveValue;
+      productElementOldPrice.textContent = priceOldValue;
+      productElementActivePriceMobile.textContent = priceActiveValue;
+      productElementOldPriceMobile.textContent = priceOldValue;
+      productElementRemains.textContent = productItem.remains;
+
+      productsList.append(productElement)
+   })
 });
+
---
+++ scripts/products.js
@@ -0,0 +1,46 @@
+
+export const initialProducts = [
+   {
+      name: 'Футболка UZcotton мужская',
+      color: 'белый',
+      size: '56',
+      img: '/images/item-1-img.svg',
+      storage: 'Коледино WB',
+      company: 'OOO Вайлдберриз',
+      remains: 'Осталось 2 шт.',
+      activePrice: '522',
+      oldPrice: '1051',
+      count: 1,
+      maxCount: 2,
+      minCount: 1
+   },
+   {
+      name: 'Силиконовый чехол картхолдер (отверстия) для карт, прозрачный кейс бампер на Apple iPhone XR, MobiSafe',
+      color: 'прозрачный',
+      size: '',
+      img: '/images/item-2-img.svg',
+      storage: 'Коледино WB',
+      company: 'OOO Мегапрофстиль',
+      remains: '',
+      activePrice: '10500',
+      oldPrice: '11500',
+      count: 200,
+      maxCount: 20000,
+      minCount: 1
+   },
+   {
+      name: 'Карандаши цветные Faber-Castell "Замок", набор 24 цвета, заточенные, шестигранные, Faber-Castell ',
+      color: '',
+      size: '',
+      img: '/images/item-3-img.svg',
+      storage: 'Коледино WB',
+      company: 'OOO Вайлдберриз',
+      activePrice: '247',
+      remains: 'Осталось 2 шт.',
+      oldPrice: '475',
+      count: 2,
+      maxCount: 2,
+      minCount: 1
+   }
+]
+
