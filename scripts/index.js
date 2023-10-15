f50b25c3f5674539bfb8aa7a20a7dd4db429dc61
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-10-15 23:14:06 +0300

    total back-info

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,18 @@
 const deliveryBackInfo = document.querySelector('.delivery__back-info');
 const deliveryBackFreeText = document.querySelector('.delivery__back-free');
+const deliveryBackInfoTotal = document.querySelector('.total .delivery__back-info');
+const deliveryBackFreeTextTotal = document.querySelector('.total .delivery__back-free');
 
 deliveryBackFreeText.addEventListener('mouseenter', function () {
    deliveryBackInfo.classList.add('display');
 })
 deliveryBackFreeText.addEventListener('mouseleave', function () {
    deliveryBackInfo.classList.remove('display');
+})
+
+deliveryBackFreeTextTotal.addEventListener('mouseenter', function () {
+   deliveryBackInfoTotal.classList.add('display');
+})
+deliveryBackFreeTextTotal.addEventListener('mouseleave', function () {
+   deliveryBackInfoTotal.classList.remove('display');
 })
